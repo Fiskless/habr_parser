60ffd032d0db0a9475b8f8d8ba5da64a813fc098
Fiskless <fiskless@gmail.com>
2022-11-20 22:08:59 +0500

    add readme and rename script

--- habr_parser.py
+++ habr_parser.py
@@ -22,7 +22,7 @@ def create_argument_parser():
         '--csv_url',
         help='Укажите ссылку на csv, в котором лежат url-ы страниц с хабра',
         nargs='?',
-        default='https://drive.google.com/file/d/1oIAXcPzyxpTEj5hdZ8bZPA4TPVX9k_nC/view',
+        default='https://drive.google.com/file/d/1oIAXcPzyxpTEj5hdZ8bZPA4TPVX9k_nC/view?usp=share_link',
         type=str
     )
     parser.add_argument(
@@ -69,7 +69,7 @@ def parse_data_from_habr(url, user_agent, proxy):
         'https': env.str('SOCKS5_PROXY')
     }
 
-    response = requests.get(url, headers=headers, proxies=proxies, timeout=10)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     page_html = response.text
     soup = BeautifulSoup(page_html, 'lxml')
@@ -77,21 +77,23 @@ def parse_data_from_habr(url, user_agent, proxy):
     return url, username, page_html
 
 
-def read_csv_from_google_drive(url='https://drive.google.com/file/d/1oIAXcPzyxpTEj5hdZ8bZPA4TPVX9k_nC/view'):
+def read_csv_from_google_drive(url):
     path = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
     user_urls = pd.read_csv(path).values
     return user_urls
 
 
 def upload_data_to_csv(user_link, user_agent, proxy):
-
-    url, username, page_html = parse_data_from_habr(user_link[0],
-                                                    user_agent,
-                                                    proxy)
-    with open(f'Страницы с хабра/{username}.csv', 'w') as f:
-        writer = csv.writer(f)
-        writer.writerows([[url], [username], [page_html]])
-    time.sleep(1)
+    try:
+        url, username, page_html = parse_data_from_habr(user_link[0],
+                                                        user_agent,
+                                                        proxy)
+        with open(f'Страницы с хабра/{username}.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows([[url], [username], [page_html]])
+        time.sleep(1)
+    except Exception as e:
+        print(e)
 
 
 def main():
@@ -102,7 +104,7 @@ def main():
     futures = []
     os.makedirs(f'{args.dest_folder}/Страницы с хабра', exist_ok=True)
 
-    # for user_link in read_csv_from_google_drive():
+    # for user_link in read_csv_from_google_drive(args.csv_url):
     #     upload_data_to_csv(user_link, choice(user_agents), 'choice(proxies)')
     with ThreadPoolExecutor() as executor:
         for user_link in read_csv_from_google_drive(args.csv_url):
